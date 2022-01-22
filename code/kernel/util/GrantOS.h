f612df2beebb819409efbe56c995917b411b6e12
GaryTheBlobfish <83780368+GaryTheBlobfish@users.noreply.github.com>
2022-01-22 21:32:51 +0000

    Create GrantOS.h

---
+++ code/kernel/util/GrantOS.h
@@ -0,0 +1,10 @@
+
+// SYSTEM VARIABLES
+extern int currentCharPrintOffset;
+
+
+// SYSTEM FUNCTIONS
+
+// put a character onto the screen at the position given by the offset (from the top left of the screen)
+extern void charAt(char c, int offset);
+extern void print(char *s, int s_len);
